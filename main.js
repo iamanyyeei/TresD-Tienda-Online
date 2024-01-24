b89afd3caad0fa4e30ebe2f620892d6eb59c7401
Any Yeilinet <iamanyyeei@gmail.com>
2024-01-23 22:29:12 -0400

    Menú mobile y menú desktop fucionados

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuIcon = document.querySelector('.menu');
+const mobileMenuWeb = document.querySelector('.mobileMenuWeb');
+
 /*Mostrar funcion y quitar*/
 navEmail.addEventListener('click',toogleDesktopMenu);
+menuIcon.addEventListener('click',toogleMobileMenuWeb);
 
 function toogleDesktopMenu(){
     console.log('click');
     desktopMenu.classList.toggle('inactive');
-}
+};
 
-console.log('Funcional');
+function toogleMobileMenuWeb(){
+    console.log('click');
+    mobileMenuWeb.classList.toggle('inactive');
+};
